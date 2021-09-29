f41721ea7f6613e02f30d3a01f4faab344c7dd1c
Auezkhan Daniyar <nurovich14@gmail.com>
2021-09-29 19:26:22 +0600

    async functions

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
 
 class ChooseLocation extends StatefulWidget {
-  ChooseLocation({Key? key}) : super(key: key);
-
   @override
   _ChooseLocationState createState() => _ChooseLocationState();
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  // async function
+  void getData() async {
+    // simulate network request for a username
+    await Future.delayed(const Duration(seconds: 3), () {
+      print('yoshi');
+    });
+    print('Hello world');
+  }
+
+  // LifeCycle: on mounting component
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
+
+  // LifeCycle: on every update of the component
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text('Choose Location Screen'));
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Choose your location'),
+        ),
+        body: const Text('Choose Location Screen'));
   }
 }
