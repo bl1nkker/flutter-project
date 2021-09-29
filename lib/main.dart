a617f266c5904132b47e8a894ee342d8add04bcd
Auezkhan Daniyar <nurovich14@gmail.com>
2021-09-29 14:41:24 +0600

    added fonts and custom class

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Text("Hello world"),
-  ));
+  runApp(const MaterialApp(home: Home()));
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Hello world',
+          style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              // ! fonts initializes on pubspec.yaml file
+              fontFamily: 'StickNoBills',
+              letterSpacing: 2.0,
+              color: Colors.green),
+        ),
+        // Centralize the title
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: const Center(
+        child: Text("Hello my guys!"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => [],
+        child: const Text('Click Me!'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
 }
